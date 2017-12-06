06e0a8f29507f262a1c373b522f1ab40239114ce
Miguel Salas <miguel@kundigo.pro>
2017-12-05 22:24:11 -0400

    fix:  return only the field about the last update timestamp.

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ const api = {
     axios.get(`${appUrl}${CHECK_SUM_END_POINT}`)
       .then(
         (response) => (response.data)
-      ).then(data => data)
+      ).then(data => data.app_md5_checksum_updated_at)
   )
 };
 
