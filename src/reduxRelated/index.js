5069dcca947dea2f5d90cd29b44cc5160ac14b66
Miguel Salas <miguel@kundigo.pro>
2017-12-06 00:24:05 -0400

    feat: executes the update function for every domain in the redux state every second.

--- src/reduxRelated/index.js
+++ src/reduxRelated/index.js
@@ -1,10 +1,14 @@
 import {createStore} from 'redux';
-import rootReducer from './reducers';
-
 import {wrapStore} from 'react-chrome-redux';
+import rootReducer from './reducers';
+import updateDomainsData from './update/domains'
 
 const store = createStore(rootReducer, {});
 
 wrapStore(store, {
   portName: 'example'
 });
+
+const updateIntervalId = setInterval( function () {
+  updateDomainsData(store)
+}, 1000);
