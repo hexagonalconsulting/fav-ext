ad1684aceb9a35231219005fae3bbb9984de484b
Miguel Salas <miguel@kundigo.pro>
2017-12-06 20:45:48 -0400

    chore: adds a toggleAutorefresh action creator.

--- src/reduxRelated/actions/index.js
+++ src/reduxRelated/actions/index.js
@@ -1,9 +1,18 @@
 export const UPDATE_SITE = 'UPDATE_SITE';
+export const TOGGLE_AUTOREFRESH = 'TOGGLE_AUTOREFRESH';
 
 export default function updateSite({ site, lastUpdated }) {
   return {
     type: UPDATE_SITE,
     site,
     lastUpdated,
+  }
+}
+
+export function toggleAutoRefresh({ site, autoRefresh }) {
+  return {
+    type: TOGGLE_AUTOREFRESH,
+    site,
+    autoRefresh,
   }
 }
