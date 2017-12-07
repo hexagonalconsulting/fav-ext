79b894a9bbbb9d9786508146e583f016607b77ca
Miguel Salas <miguel@kundigo.pro>
2017-12-07 00:37:39 -0400

    feat: sets and gets the auto refresh toggle value in the redux state. notes: this commit does a lot of little things.  * sets auto refresh value calling the action creator.  * sets up a function to trigger the refresh of the page.  * puts some order in the set up function AKA camponentWillMount.  * request the tabId from the background script sendinfg a message.  * sets up a rather clumsy logic in the render method and in the    mapStateToProps function to avoid errors when the redux state is    not yet set up.

--- src/content/components/app/App.js
+++ src/content/components/app/App.js
@@ -2,61 +2,85 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import ToggleButton from 'react-toggle-button'
 import api from '../../../utils/api'
-import updateSite from '../../../reduxRelated/actions/index'
+import updateSite, {toggleAutoRefresh} from '../../../reduxRelated/actions/index'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       lastUpdated: null,
-      autoRefresh:  false
-    }
+      tabId: false
+    };
+    this.domain = document.location.origin
   }
 
-  toggleAutoRefresh = () => {
-    const { autoRefresh} = this.state;
-    this.setState({ autoRefresh: !autoRefresh })
+  _toggleAutoRefresh = (autoRefresh) => {
+    this.props.dispatch( toggleAutoRefresh({ site: this.domain , autoRefresh: !autoRefresh }) );
   };
 
-  shouldAutoRefresh = (lastUpdated) => {
-    const { lastUpdated: tabLastUpdated, autoRefresh }  = this.state;
-
-    if ( autoRefresh && ( lastUpdated !== tabLastUpdated ) ) {
-      location.reload(true)
+  shouldAutoRefresh = (lastUpdated, autoRefresh) => {
+    const { lastUpdated: tabLastUpdated }  = this.state;
+    if (
+      !(lastUpdated === null || tabLastUpdated === null) && // Only if neither is null,
+       (autoRefresh && ( lastUpdated !== tabLastUpdated) )  // and auto refresh is true and they are different.
+    ) {
+      location.reload(true);
     }
 
   };
 
-  componentWillMount() {
-
-    api.fetchChecksumLastUpdatedAt(document.location.origin)
+  fetchAppLastUpdatedTimestamp = () =>  {
+    api.fetchChecksumLastUpdatedAt(this.domain)
       .then(
         lastUpdated => {
 
-          const site = document.location.origin;
-          this.props.dispatch(updateSite({ site , lastUpdated }));
+          this.props.dispatch(updateSite({ site: this.domain , lastUpdated }));
           this.setState({lastUpdated})
 
         }
       );
+  };
+
+  requestTabId = () => {
+    // This ask the background page, (in our case a script) for the tab id in which the component is running.
+    chrome.runtime.sendMessage({ request: "get tabId" }, ({tabId}) => {
+      this.setState({tabId});
+    })
+  };
+
+  componentDidMount() {
+
+    const {fetchAppLastUpdatedTimestamp, requestTabId} = this;
+
+    fetchAppLastUpdatedTimestamp();
+
+    requestTabId()
+
   }
 
   render() {
 
-    const domains = this.props.domains;
-    const domain = document.location.origin;
-    const lastUpdated = domains[domain] ? domains[domain].lastUpdated : null;
-    const { lastUpdated: tabLastUpdated, autoRefresh }  = this.state;
-    const { toggleAutoRefresh, shouldAutoRefresh } = this;
+    const {domains, tabs} = this.props;
+    const {domain} = this;
+    let lastUpdated, autoRefresh;
 
-    shouldAutoRefresh(lastUpdated);
+    const { _toggleAutoRefresh, shouldAutoRefresh } = this;
+    const { lastUpdated: tabLastUpdated, tabId }  = this.state;
+
+    if (domains && domains[domain]) {
+      lastUpdated = domains[domain].lastUpdated;
+      autoRefresh = (  (tabs  || {} )[tabId]  || {}  ).autoRefresh;
+      autoRefresh =   !!autoRefresh ;
+      shouldAutoRefresh(lastUpdated, autoRefresh);
+    }
+
 
     return (
       <div>
         App last updated: { lastUpdated }, Tab last updated: {tabLastUpdated} {lastUpdated === tabLastUpdated && 'Up to date'}, Auto refresh :
         <ToggleButton
           value={ autoRefresh }
-          onToggle={ toggleAutoRefresh }
+          onToggle={ () => _toggleAutoRefresh(autoRefresh) }
         />
       </div>
     );
@@ -64,9 +88,14 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
+
+  const domain = document.location.origin;
+  const {domains} = state;
+  const thereIsADomain = (domains || {} )[domain];
   return {
-    domains: state.domains ? state.domains : {}
-  };
+    domains: domains ? domains : {},
+    tabs: (thereIsADomain || {})['tabs']
+  }
 };
 
 export default connect(mapStateToProps)(App);
