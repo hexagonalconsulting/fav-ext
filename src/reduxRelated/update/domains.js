d9cef639507e3f2b6378e5c523dad2bb3bc3e547
Miguel Salas <miguel@kundigo.pro>
2017-12-08 17:08:17 -0400

    feat: perform autoUpdate only if the autoUpdate boolean field is true (for the specific domain). That is, if the toggle has been set to on.

--- src/reduxRelated/update/domains.js
+++ src/reduxRelated/update/domains.js
@@ -2,11 +2,21 @@ import api  from '../../utils/api'
 import updateSite from '../actions/index'
 
 export default function (store) {
+
   const domains = store.getState().domains ? store.getState().domains : {} ;
-  Object.keys(domains).forEach( site => (
-    api.fetchChecksumLastUpdatedAt(site)
-      .then(
-        lastUpdated => store.dispatch( updateSite({site, lastUpdated}) )
-      )
-  ))
+
+  Object.keys(domains).forEach( site => {
+
+    const domainData = domains[site];
+    // If autoUpdate key does not exist yet, then it has not been set for the first time, we will get `undefined`,
+    // so !!undefined will turn that into a boolean : false .
+    const autoUpdate = !!domainData.autoUpdate;
+
+    if (autoUpdate) {
+      api.fetchChecksumLastUpdatedAt(site)
+        .then(
+          lastUpdated => store.dispatch(updateSite({site, lastUpdated}))
+        )
+    }
+  })
 }
