431efc805bd55a28a72221734870e3a581103f5c
Miguel Salas <miguel@kundigo.pro>
2017-12-12 18:17:50 -0400

    chore: sets up an action creator and const SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT for the case, when an listener has been set for watching a specific tab close event.

--- src/reduxRelated/actions/index.js
+++ src/reduxRelated/actions/index.js
@@ -2,6 +2,7 @@ export const UPDATE_SITE = 'UPDATE_SITE';
 export const TOGGLE_AUTOREFRESH = 'TOGGLE_AUTOREFRESH';
 export const TOGGLE_UPDATES_FROM_SITE = 'TOGGLE_UPDATES_FROM_SITE';
 export const DELETE_TAB_DATA = 'DELETE_TAB_DATA';
+export const SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT = 'SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT';
 
 export default function updateSite({ site, lastUpdated }) {
   return {
@@ -29,6 +30,14 @@ export function toggleAutoUpdate({ site, autoUpdate }) {
 export function deleteTabData({ site, tabId}) {
   return {
     type: DELETE_TAB_DATA,
+    site,
+    tabId,
+  }
+}
+
+export function setTabAsWatchedForTabClosedEvent({ site, tabId}) {
+  return {
+    type: SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT,
     site,
     tabId,
   }
