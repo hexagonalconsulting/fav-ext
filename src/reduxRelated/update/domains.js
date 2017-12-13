630227ebd4c5a238ab7cb86fbca912f730506c3a
Miguel Salas <miguel@kundigo.pro>
2017-12-12 20:26:45 -0400

    feat: does not perform auto update if the site to auto update does not have at least one tab open.

--- src/reduxRelated/update/domains.js
+++ src/reduxRelated/update/domains.js
@@ -11,8 +11,12 @@ export default function (store) {
     // If autoUpdate key does not exist yet, then it has not been set for the first time, we will get `undefined`,
     // so !!undefined will turn that into a boolean : false .
     const autoUpdate = !!domainData.autoUpdate;
+    // The boolean domainDataHasAtLeastOneTabOpen indicates that this domain has at least a tabId key in the redux state.
+    // That is, the 'tabs' key exist in this domain data, and has at least one key (keys there represent tabIds objects) inside.
+    const domainDataHasAtLeastOneTabOpen =  !!((domainData || {})['tabs']) && ( Object.keys(domainData['tabs']).length > 0);
+    // So if domainDataHasAtLeastOneTabOpen is false, the auto update should not be performed.
 
-    if (autoUpdate) {
+    if (autoUpdate && domainDataHasAtLeastOneTabOpen) {
       api.fetchChecksumLastUpdatedAt(site)
         .then(
           lastUpdated => store.dispatch(updateSite({site, lastUpdated}))
