8db3aade2a9f27fb1359d8a05b9795751046fa63
Miguel Salas <miguel@kundigo.pro>
2017-12-13 22:16:20 -0400

    fix: setup cont SET_TAB_AS_WATCHED_FOR_TAB_UPDATED_EVENT and related action creator.

--- src/reduxRelated/actions/index.js
+++ src/reduxRelated/actions/index.js
@@ -3,6 +3,7 @@ export const TOGGLE_AUTOREFRESH = 'TOGGLE_AUTOREFRESH';
 export const TOGGLE_UPDATES_FROM_SITE = 'TOGGLE_UPDATES_FROM_SITE';
 export const DELETE_TAB_DATA = 'DELETE_TAB_DATA';
 export const SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT = 'SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT';
+export const SET_TAB_AS_WATCHED_FOR_TAB_UPDATED_EVENT = 'SET_TAB_AS_WATCHED_FOR_TAB_UPDATED_EVENT';
 
 export default function updateSite({ site, lastUpdated }) {
   return {
@@ -39,6 +40,14 @@ export function setTabAsWatchedForTabClosedEvent({ site, tabId}) {
   return {
     type: SET_TAB_AS_WATCHED_FOR_TAB_CLOSED_EVENT,
     site,
+    tabId,
+  }
+}
+
+export function setTabAsWatchedForTabUpdatedEvent({ domain, tabId}) {
+  return {
+    type: SET_TAB_AS_WATCHED_FOR_TAB_UPDATED_EVENT,
+    domain,
     tabId,
   }
 }
