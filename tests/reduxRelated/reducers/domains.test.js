b3c2d768087e00e06af49263fa1be0da62c6bd76
Miguel Salas <miguel@kundigo.pro>
2017-12-22 15:02:31 -0400

    fix: typo in the expectation description.

--- tests/reduxRelated/reducers/domains.test.js
+++ tests/reduxRelated/reducers/domains.test.js
@@ -4,7 +4,7 @@ let  expectedNextState, previousState, action, reducerOutput;
 
 describe('UPDATE_SITE  action', () =>{
 
-  test('it updates the update site field in the  related domain object', () => {
+  test('it updates the lastUpdated field in the related domain object', () => {
     action = updateSite({site: 'http://mydomain.com', lastUpdated: "2017-12-22T19:43:24+01:00"});
 
     previousState = {
