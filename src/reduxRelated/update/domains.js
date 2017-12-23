d2709bb8ce48e673a3f6569d13b8b1f33ccc7482
Miguel Salas <miguel@kundigo.pro>
2017-12-23 02:14:28 -0400

    fix: update function takes in account the new shape of the state.

--- src/reduxRelated/update/domains.js
+++ src/reduxRelated/update/domains.js
@@ -3,18 +3,16 @@ import updateSite from '../actions/index'
 
 export default function (store) {
 
-  const domains = store.getState().domains ? store.getState().domains : {} ;
+  const {domains, domainsIds} = store.getState() ;
 
-  Object.keys(domains).forEach( site => {
+  domainsIds.forEach( site => {
 
     const domainData = domains[site];
     // If autoUpdate key does not exist yet, then it has not been set for the first time, we will get `undefined`,
     // so !!undefined will turn that into a boolean : false .
     const autoUpdate = !!domainData.autoUpdate;
-    // The boolean domainDataHasAtLeastOneTabOpen indicates that this domain has at least a tabId key in the redux state.
-    // That is, the 'tabs' key exist in this domain data, and has at least one key (keys there represent tabIds objects) inside.
-    const domainDataHasAtLeastOneTabOpen =  !!((domainData || {})['tabs']) && ( Object.keys(domainData['tabs']).length > 0);
-    // So if domainDataHasAtLeastOneTabOpen is false, the auto update should not be performed.
+
+    const domainDataHasAtLeastOneTabOpen =  domainData.tabsIds.length > 0;
 
     if (autoUpdate && domainDataHasAtLeastOneTabOpen) {
       api.fetchChecksumLastUpdatedAt(site)
