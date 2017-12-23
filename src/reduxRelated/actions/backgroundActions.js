fc8d08d0a3cd9b65c3771ae2aaa38f255f782e4d
Miguel Salas <miguel@kundigo.pro>
2017-12-23 00:06:28 -0400

    wip: asyncWatchForTabUpdated, asyncWatchForClosedTab improve a bit the code by taking in account the new state shape but this is not backed yet by any test.

--- src/reduxRelated/actions/backgroundActions.js
+++ src/reduxRelated/actions/backgroundActions.js
@@ -10,24 +10,18 @@ function asyncWatchForClosedTab(action) {
 
   return function (dispatch, getState) {
 
-    const state = getState();
-    const { domains } = state;
+    const { tabs, tabsIds } = getState();
     const { domain } = action;
     const {id: tabId } = action._sender.tab;
 
-    // this set of variables is used to determine if  the boolean `watchedForCloseEvent` for this tabId
-    // is present in the  redux state. Expressions with the structure: `(object || {})[property]`
-    // are there to avoid errors caused by trying to read properties not existent in the related object,
-    // double negations (!!) are there to take the boolean value if the result of the expression is undefined: !!undefined => false.
-    const thereIsDomainData = !!(domains || {})[domain];
-    const thereIsTabsData =  !!(thereIsDomainData &&  (domains[domain] || {})['tabs']);
-    const thereIsDataForThisTab =  !!(thereIsTabsData &&  (domains[domain]['tabs'] || {})[tabId]);
-    const tabIsAlreadyWatchedForCloseEvent =  !!(thereIsDataForThisTab &&  (domains[domain]['tabs'][tabId] || {})['watchedForCloseEvent']);
+    let tabIsAlreadyWatchedForCloseEvent = tabsIds.includes(tabId)
+      ? !!tabs[tabId].watchedForCloseEvent
+      : false;
 
     if (!tabIsAlreadyWatchedForCloseEvent) {
 
         addListenerForTabCloseEvent({ tabId, dispatch, site: domain});
-        dispatch( setTabAsWatchedForTabClosedEvent({ tabId, site: domain}) )
+        dispatch( setTabAsWatchedForTabClosedEvent({ tabId }) )
 
     }
   }
@@ -64,23 +58,18 @@ function addListenerForTabUpdatedEvent({ tabId, dispatch, domain}) {
 function asyncWatchForTabUpdated(action) {
   return function (dispatch, getState) {
 
-    const state = getState();
-    const { domains } = state;
+    const { tabs, tabsIds  } = getState();
     const { domain } = action;
     const {id: tabId } = action._sender.tab;
 
-    // this set of variables is used to determine if  the boolean `watchedForUpdatedEvent` for this tabId
-    // is present in the  redux state. Expressions with the structure: `(object || {})[property]`
-    // are there to avoid errors caused by trying to read properties not existent in the related object,
-    // double negations (!!) are there to take the boolean value if the result of the expression is undefined: !!undefined => false.
-    const thereIsDomainData = !!(domains || {})[domain];
-    const thereIsTabsData =  !!(thereIsDomainData &&  (domains[domain] || {})['tabs']);
-    const thereIsDataForThisTab =  !!(thereIsTabsData &&  (domains[domain]['tabs'] || {})[tabId]);
-    const tabIsAlreadyWatchedForUpdatedEvent =  !!(thereIsDataForThisTab &&  (domains[domain]['tabs'][tabId] || {})['watchedForUpdatedEvent']);
+    let tabIsAlreadyWatchedForUpdatedEvent = tabsIds.includes(tabId)
+      ? !!tabs[tabId].watchedForUpdatedEvent
+      : false;
+
     if (!tabIsAlreadyWatchedForUpdatedEvent) {
 
       addListenerForTabUpdatedEvent({ tabId, dispatch, domain});
-      dispatch( setTabAsWatchedForTabUpdatedEvent({ tabId, site: domain }) )
+      dispatch( setTabAsWatchedForTabUpdatedEvent({ tabId }) )
 
     }
   }
