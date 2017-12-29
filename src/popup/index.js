574356b0ab77105bba1943d0aa9f82e051fd5a8a
Miguel Salas <miguel@kundigo.pro>
2017-12-29 18:13:40 -0400

    fix: render only when the proxy store gets the real state of store.

--- src/popup/index.js
+++ src/popup/index.js
@@ -8,9 +8,13 @@ const proxyStore = new Store({
   portName: 'example'
 });
 
-render(
-    <Provider store={proxyStore}>
-      <App/>
-    </Provider>
-  , document.getElementById('app')
-);
+// this block makes sure that App is not render until the proxy store gets the initialState from the real store.
+const unsubscribe = proxyStore.subscribe(() => {
+	unsubscribe(); // make sure to only fire once
+  render(
+      <Provider store={proxyStore}>
+        <App domain={activeTabDomain} />
+      </Provider>
+    , document.getElementById('app')
+  );
+});
